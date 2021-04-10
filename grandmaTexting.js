64c6d26ae1bcd390d7fa25fe52a77a317360f486
fpolanco <polanco.franchell@gmail.com>
2021-04-09 23:30:46 -0400

    2 added

---
+++ grandmaTexting.js
@@ -0,0 +1,11 @@
+function textin(str){
+    // identify the three words to turn to 2
+      //find the method to change
+      //look through string google
+      //if we find them them change to 2
+      //return new string with replaced 2
+      console.log(str)
+      result = str.replace(/too|two|to/gi, 2)
+        return result
+      
+    }
