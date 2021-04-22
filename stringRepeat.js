98e8d2bf3638bc128e5630fc81e70b313860dfa8
fpolanco <polanco.franchell@gmail.com>
2021-04-22 16:51:22 -0400

    repeat string

---
+++ stringRepeat.js
@@ -0,0 +1,10 @@
+//Write a function called repeatStr which 
+//repeats the given string string exactly n times.
+
+function repeatStr (n, s) {
+    //input is a string
+    // output string n times
+    let repeat = s.repeat(n)
+    return repeat;
+  }
+
